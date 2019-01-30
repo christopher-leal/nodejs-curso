3c1a66e3320a46c67ead863e6e170b85f99ac7c3
Christopher Leal <chris_t_o@hotmail.com>
2019-01-29 23:20:25 -0600

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-
 /**
  * Puerto
  */
-process.env.PORT= process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * Entorno
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+/**
+ * Base de datos
+ */
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB ='mongodb://cafe-user:abc123@ds011495.mlab.com:11495/cafe-udemy'
+}
+
+process.env.URLDB = urlDB;
